7589f1477b252d518b9243515e57e5d00ee78d41
daneklustig <danek.lustig@gmail.com>
2019-11-05 13:46:09 +0100

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,55 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+  }
+  get(pos) {
+    if (this.length < pos) {
+      throw new Error("OutOfBounds")
+    } else {
+      return this.items[pos];
+    }
+
+  }
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return Math.max(...this.items)
+    }
+  }
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return Math.min(...this.items)
+    }
+
+
+  }
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((a, b) => a + b)
+    }
+  }
+
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      // return this.items.reduce((a, b) => (a + b) / this.items.length)
+      return this.sum() / this.length
+    }
+  }
+
+};
+
+module.exports = SortedList;
